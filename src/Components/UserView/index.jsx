e514c72e527ef41fb4b0226dbed417ee51673a99
DilanMDA <akiladilan04@gmail.com>
2022-07-19 13:57:40 +0530

    changes

--- src/Components/UserView/index.jsx
+++ src/Components/UserView/index.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const UserView = ()=>{
-    return(<h1>
-        Users
-    </h1>)
-}
+const UserView = () => {
+  return <div>Users</div>;
+};
 
 export default UserView;
